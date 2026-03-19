7fd5a789daf7ee26a964b42d9423d5bea96ba2f8
Ryan Martin <ryanmartin@uidaho.edu>
2026-03-19 14:57:27 -0700

    Add startup message via .onAttach()

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "ecologytools ", utils::packageVersion("ecologytools"), "\n",
+    "Utilities and Data Visualization Tools for Ecology\n",
+    "Functions: theme_martin(),\n vrml(),\n dist_escape(),\n days_since_peak_IRG(),\n aggregate_raster()"
+  )
+}
