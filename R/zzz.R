41a77254b19735d4b85af97a5d8928ceed334951
Ryan Martin <ryanmartin@uidaho.edu>
2026-04-01 13:52:43 -0700

    updated start up message

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,6 @@
   packageStartupMessage(
     "ecologytools ", utils::packageVersion("ecologytools"), "\n",
     "Utilities and Data Visualization Tools for Ecology\n",
-    "Functions: theme_martin(),\n vrml(),\n dist_escape(),\n days_since_peak_IRG(),\n aggregate_raster()"
+    "Functions: theme_martin(),\n vrml(),\n dist_escape(),\n days_since_peak_IRG(),\n aggregate_raster(), \n sheep_cluster()"
   )
 }
